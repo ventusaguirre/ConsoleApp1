4fc1ee310dbb8a46c67541dc949f63c568f1a928
ventusaguirre <ventusaguirre@my.whccd.edu>
2026-01-15 14:44:11 -0800

    changed "World" to "People"

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace Comp003A
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, People!");
         }
     }
 }
