832a92a73ad2435d629a0c9f4e88aa2fc6d6a91f
vmagu <vmagu@V-INSPIRON>
2026-01-15 14:39:46 -0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Comp003A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
